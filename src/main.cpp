69edaf5b4ca2e28a4288ea1dd454b64712cb022f
Dmitry Kutsenko <d2emonium@gmail.com>
2016-11-30 22:07:16 +0300

    Loading textures

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main()
 {
     // Create the main window
     sf::RenderWindow app(sf::VideoMode(winSize[0], winSize[1]), gameTitle);
+    app.setFramerateLimit(fps);
 
     // Load a sprite to display
     int backgroundId = rand() % backgroundsCount;
@@ -13,6 +14,32 @@ int main()
     if (!tBackground.loadFromFile(backgrounds[backgroundId]))
         return EXIT_FAILURE;
     sf::Sprite sBackground(tBackground);
+
+    sf::Texture tBlock;
+    if (!tBlock.loadFromFile(blocks[0]))
+        return EXIT_FAILURE;
+    sf::Sprite sBlock[1000]
+    // (tBlock);
+    int n = 0;
+    for(int i=0; i<10; i++)
+        for(int j=0; j<10; j++)
+        {
+            sBlock[n].setTexture(tBlock);
+            sBlock[n].setPosition(i * 43, j * 20);
+            n++;
+        }
+
+    sf::Texture tBall;
+    if (!tBall.loadFromFile(ball))
+        return EXIT_FAILURE;
+    sf::Sprite sBall(tBall);
+    sBall.setPosition(300, 300);
+
+    sf::Texture tPaddle;
+    if (!tPaddle.loadFromFile(paddle))
+        return EXIT_FAILURE;
+    sf::Sprite sPaddle(tPaddle);
+    sPaddle.setPosition(300, 440);
 
 	// Start the game loop
     while (app.isOpen())
--- src/settings.hpp
+++ src/settings.hpp
@@ -6,11 +6,12 @@
 sf::String gameTitle = "Rerkanoid";
 int winSize[2] = {800, 600};
 
-// const int M=20;
-// const int N=10;
+const int M = 450;
+const int N = 520;
 // int boxPosition[2] = {28, 18};
-// int boxSize[2] = {N, M};
+int boxSize[2] = {N, M};
 // int pointSize[2] = {18, 18};
+int fps = 60;
 
 int backgroundsCount = 10;
 sf::String backgrounds[10] = {
@@ -25,6 +26,17 @@ sf::String backgrounds[10] = {
     "res/background/9.png",
     "res/background/10.png",
 };
+
+sf::String blocks[5] = {
+    "res/block01.png",
+    "res/block02.png",
+    "res/block03.png",
+    "res/block04.png",
+    "res/block05.png",
+};
+
+sf::String ball = "res/ball.png";
+sf::String paddle = "res/paddle.png";
 
 // sf::String interface = "res/frame.png";
 
