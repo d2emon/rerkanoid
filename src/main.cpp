3ba8e1e08d988fa5b98fcf5bc18a00b03a20acf5
Dmitry Kutsenko <d2emonium@gmail.com>
2016-12-01 20:59:10 +0300

    Fill field

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 int main()
 {
+    srand(time(0));
+
     // Create the main window
     sf::RenderWindow app(sf::VideoMode(winSize[0], winSize[1]), gameTitle);
     app.setFramerateLimit(fps);
@@ -18,11 +20,12 @@ int main()
     sf::Texture tBlock;
     if (!tBlock.loadFromFile(blocks[0]))
         return EXIT_FAILURE;
-    sf::Sprite sBlock[1000]
+    sf::Sprite sBlock[1000];
     // (tBlock);
+
     int n = 0;
-    for(int i=0; i<10; i++)
-        for(int j=0; j<10; j++)
+    for(int i=1; i<10; i++)
+        for(int j=1; j<10; j++)
         {
             sBlock[n].setTexture(tBlock);
             sBlock[n].setPosition(i * 43, j * 20);
@@ -59,9 +62,16 @@ int main()
 
         // Draw the sprite
         app.draw(sBackground);
+        app.draw(sPaddle);
+        app.draw(sBall);
 
-        // Update the window
-        app.display();
+        for(int i=0; i<n; i++)
+        {
+            app.draw(sBlock[i]);
+        }
+
+            // Update the window
+            app.display();
     }
 
     return EXIT_SUCCESS;
